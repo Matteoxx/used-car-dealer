1db8ffba5b46a3daf78fc7a26942d8841d945be0
matteoxx <mateuszniziolek96@gmail.com>
2019-06-08 22:07:41 +0200

    car details added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { BuyComponent } from "./buy/buy.component";
 import { SellComponent } from "./sell/sell.component";
+import { CarDetailsComponent } from "./buy/car-details/car-details.component";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "kup", component: BuyComponent },
+  { path: "kup/:id", component: CarDetailsComponent },
   { path: "sprzedaj", component: SellComponent }
 ];
 
